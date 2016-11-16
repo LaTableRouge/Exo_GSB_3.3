7c63ecbfc1d5b1340ced221c6f8b83a4ae053017
Nero23 <romainstefanelli@gmail.com>
2016-11-16 14:38:07 +0100

    Update c_connexion.php

--- c_connexion.php
+++ c_connexion.php
@@ -17,13 +17,29 @@ switch($action){
 			include("vues/v_erreurs.php");
 			include("vues/v_connexion.php");
 		}
-		else{
+		else if(is_array( $visiteur)){
 			$id = $visiteur['id'];
 			$nom =  $visiteur['nom'];
 			$prenom = $visiteur['prenom'];
 			connecter($id,$nom,$prenom);
 			include("vues/v_sommaire.php");
 		}
+        // si l'admin se connecte
+        else if(is_array( $admin)){
+            $id = $admin['id'];
+			$nom =  $admin['nom'];
+			$prenom = $admin['prenom'];
+			connecter($id,$nom,$prenom);
+			include("vues/v_sommaire.php");
+        }
+        // si un comptable se connecte
+        else if(is_array( $comptable)){
+            $id = $comptable['id'];
+			$nom =  $comptable['nom'];
+			$prenom = $comptable['prenom'];
+			connecter($id,$nom,$prenom);
+			include("vues/v_sommaire.php");
+        }
 		break;
 	}
 	default :{
