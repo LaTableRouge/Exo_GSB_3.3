79a129bb91b88c9818eac584f131a582bc2cb77b
Nero23 <romainstefanelli@gmail.com>
2017-04-23 16:53:00 +0200

    Add files via upload

---
+++ GSB-Frais-Final/vues/v_pied.php
@@ -0,0 +1,14 @@
+<footer class="footer" style="padding-top: 60px;">
+  <div class="container well">
+    <p class="text-muted">Copyright &copy; 2017 GSB.</p>
+  </div>
+</footer>
+
+</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
